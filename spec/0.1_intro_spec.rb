c34c150d27d1c2056213901fed0362f8ed6a217a
Bartek Wilczek <bwilczek@gmail.com>
2018-07-16 22:58:16 +0200

    Added more test pages

--- spec/0.1_intro_spec.rb
+++ spec/0.1_intro_spec.rb
@@ -8,6 +8,7 @@ end
 RSpec.describe IndexPage do
   it 'displays header' do
     IndexPage.open do
+      expect(header).to be_instance_of Watir::Heading
       expect(header.text).to eq 'Welcome to WatirPump tutorial'
     end
   end
